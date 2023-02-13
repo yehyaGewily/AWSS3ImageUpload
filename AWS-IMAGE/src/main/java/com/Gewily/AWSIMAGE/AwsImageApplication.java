65c186c330409a229a615c80eeafba7148a54d20
yehyaGewily <103772686+yehyaGewily@users.noreply.github.com>
2023-02-13 14:02:05 +0200

    Add files via upload

---
+++ AWS-IMAGE/src/main/java/com/Gewily/AWSIMAGE/AwsImageApplication.java
@@ -0,0 +1,13 @@
+package com.Gewily.AWSIMAGE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsImageApplication.class, args);
+	}
+
+}
---
+++ AWS-IMAGE/src/test/java/com/Gewily/AWSIMAGE/AwsImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Gewily.AWSIMAGE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
